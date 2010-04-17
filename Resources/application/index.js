98f1be7ebda0cc89cd523daba00d9a30a1ec02ff
Łukasz Korecki <lukasz@coffeesounds.com>
2010-04-17 15:40:32 +0100

    Keyboard events Code cleanup Switched from keyup to keydown - however it can't "return false"  - hence the If at the end of the Dispatcher

--- Resources/application/index.js
+++ Resources/application/index.js
@@ -21,13 +21,13 @@ document.observe(
   function(){
     Application.loadWindowSettings();
     Application.cache_start();
-      $('input_area').toggle(); // <- this needs to be a function
-      $('menubar').toggle();
+    $('input_area').toggle(); // <- this needs to be a function
+    $('menubar').toggle();
 
 
     // TEH DISPACHA
 
-    // for dispatcher to work few conventions need to followed:
+    // for Dispatcher to work few conventions need to followed:
     // element name can be what ever, like "blomp" or "blamp_user"
     // for elemen/button "blomp_user" the event hadnler will be "Events.blomp_user"
     // if the link/button is containing an image (as a icon), the img elements
@@ -35,61 +35,48 @@ document.observe(
     // that's it, the rest is magic
 
     //
-    try {
-	    document.getElementById('wrapper').addEventListener('click',dispatcher);
-    } catch(err) {
-      // ignore
-    }
-    try {
-	    document.getElementById('wrapper').addEventListener('submit',dispatcher);
+	  document.body.observe('click',Dispatcher);
+	  document.getElementById('wrapper').addEventListener('submit',Dispatcher);
+	  document.getElementById('main_textarea').addEventListener('keydown',Dispatcher);
+    //    document.getElementById('main_textarea').addEventListener('focus', function(event) {
+    //                                                                  console.log("Focusing the area");
+    //                                                                  console.dir(event);
+    //                                                                  this.setAttribute("focued", true);
+    //                                                                });
+    //      document.getElementById('main_textarea').addEventListener('blur', function(event) {
+    //                                                                  console.log("unFocusing the area");
+    //                                                                  console.dir(event);
+    //                                                                  this.setAttribute("focued", false);
+    //                                                                });
 
-    } catch(err) {
-      // ignore
-    }
-    try {
-	    document.getElementById('main_textarea').addEventListener('keyup',dispatcher);
-      document.getElementById('main_textarea').addEventListener('focus', function(event) {
-                                                                  console.log("Focusing the area");
-                                                                  console.dir(event);
-                                                                  this.setAttribute("focued", true);
-                                                                });
-      document.getElementById('main_textarea').addEventListener('blur', function(event) {
-                                                                  console.log("unFocusing the area");
-                                                                  console.dir(event);
-                                                                  this.setAttribute("focued", false);
-                                                                });
-    } catch(err) {
-      // ignore
-    }
+    document.body.addEventListener(
+      'keydown',
+      function(event){
+        console.log(event.keyCode);
+        console.dir(event);
+        switch(event.keyCode) {
+        case 38: //up
+        case 40: // down
+          KeyboardEvents.navigate_over_updates(event);
+          break;
+        case 39: // left
+          KeyboardEvents.expand_quoted(event);
+          break;
+        case 82: // r
+          KeyboardEvents.reply_to(event);
+          break;
+        default:
+          return true;
+          break;
+        }
+        return true;
+      });
 
-      document.addEventListener(
-        'keydown',
-        function(event){
-//          console.log(event.keyCode);
-//          console.log(interfaces[active_service].active_entry);
-          var collection = $$('.update');
-
-          // UP
-          if(event.keyCode == 38 && (interfaces[active_service].active_entry-1 >= 0)) {
-            console.log("[38] Wcisnalem up, czas zwiekszyc");
-            interfaces[active_service].active_entry--;
-          }
-          // DOWN
-          if(event.keyCode == 40 && interfaces[active_service].active_entry < (collection.length-1)) {
-            console.log("[40] Wcisnalem down, czas zmniejszy");
-
-            interfaces[active_service].active_entry++;
-          }
-
-          $$('.active_entry').each(function(elem){ elem.removeClassName("active_entry"); });
-          collection[interfaces[active_service].active_entry].scrollIntoViewIfNeeded();
-          collection[interfaces[active_service].active_entry].addClassName("active_entry");
-
-
-        });
   });
 
-function dispatcher(event){
+function Dispatcher(event){
+  console.log("Dispatcher");
+  console.dir(event);
 	var element = $(event.target);
 	// get the id of clicked element
 	var id = element.identify();
@@ -105,11 +92,17 @@ function dispatcher(event){
 		event.target_up = event.target.up();
 	}
 	// now... DISPATCH! ;-)
+  console.log("ID: " + id);
 	if(Events[id] !== undefined) {
 		Events[id](event);
 	} else {
 		console.log("No such function: Events." + (id || "missing id"));
 	}
-	event.preventDefault();
-  return false;
-};
+  if(event.type == "keydown") {
+    return true;
+  } else {
+	  event.preventDefault();
+    return false;
+  }
+
+}
