cc325776ae498b19596575a36a00ba23d6b074f5
Łukasz Korecki <lukasz@coffeesounds.com>
2010-05-10 20:23:27 +0100

    Removes run_tests function

--- Resources/application/index.js
+++ Resources/application/index.js
@@ -1,14 +1,3 @@
-function run_tests() {
-  var win = Titanium.UI.getCurrentWindow();
-  var w2 = win.createWindow('app://test.html');
-
-  w2.setHeight(20);
-  w2.setWidth(20);
-  w2.setResizable(false);
-  w2.open();
-
-}
-
 
 Titanium.addEventListener(Titanium.CLOSE, function(event){
                           Application.saveWindowSettings();
