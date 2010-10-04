f9e9339295bd552be5af09e3109dff9cb085989a
Łukasz Korecki <lukasz@coffeesounds.com>
2010-10-04 10:08:09 +0100

    added required headers for file upload

--- Resources/blip/blip.uploader.rb
+++ Resources/blip/blip.uploader.rb
@@ -3,5 +3,5 @@ require 'libs/rest_client'
 
 def blip_post_file url, login, password, body, file_path
   res = RestClient::Resource.new url, :user => login, :password =>password
-  res.post :update => {:body => body, :picture => File.open(file_path)}, :headers => { "User-Agent" => "Mikrob 0.1"}
+  res.post :update => {:body => body, :picture => File.open(file_path)}, :headers => { "User-Agent" => "Mikrob 0.1", 'X-Blip-Api' => '0.02', 'Accept' => 'application/json', 'X-blip-application' => 'Mikrob 0.1'}
 end
